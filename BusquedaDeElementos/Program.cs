1da2ecf4d88d1ff0bb3eddbe83463a1c1d3a2364
Carlos-Alejandro-Cob <24090636@alumno.utmetropolitana.edu.mx>
2025-03-13 22:01:13 -0600

    update

--- BusquedaDeElementos/Program.cs
+++ BusquedaDeElementos/Program.cs
@@ -1,37 +1,85 @@
-﻿using System;
+﻿// Crea un programa que busque un elemento en un arreglo bidimensional y muestre su posición en caso de encontrarlo. En caso contrario, debe mostrar un mensaje indicando que el elemento no se encuentra en el arreglo.
+//Intenta buscar un elemento que se encuentre en el arreglo y otro que no se encuentre para verificar que el programa funciona correctamente.
+//El arreglo bidimensional debe ser de tamaño 3x3 y debe contener números enteros.
+//El programa debe tener un método llamado Buscar que reciba el arreglo bidimensional y el elemento a buscar, y muestre la posición del elemento si se encuentra o un mensaje indicando que no se encuentra.
+//exepcion si el usuario ingresa un numero que ya habia ingresado antes que imprima ambos indices
+//Ejemplo de entrada:
+//Ingrese los elementos del arreglo 3x3:
+//Ingrese el elemento [0,0]: 1
+using System;
+using System.Collections.Generic;
+
+class ArregloBidimensional
+{
+    private int[,] numeros;
+    private Dictionary<int, List<(int, int)>> indices;
+
+    // Constructor que inicializa el arreglo y el diccionario de índices
+    public ArregloBidimensional()
+    {
+        numeros = new int[3, 3];
+        indices = new Dictionary<int, List<(int, int)>>();
+    }
+
+    // Método para ingresar los elementos del arreglo
+    public void IngresarElementos()
+    {
+        Console.WriteLine("Ingrese los elementos del arreglo 3x3:");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"Ingrese el elemento [{i},{j}]: ");
+                int numero = int.Parse(Console.ReadLine());
+                numeros[i, j] = numero;
+
+                // Almacenar el índice del número en el diccionario
+                if (indices.ContainsKey(numero))
+                {
+                    indices[numero].Add((i, j));
+                }
+                else
+                {
+                    indices[numero] = new List<(int, int)> { (i, j) };
+                }
+            }
+        }
+    }
+
+    // Método para buscar un elemento en el arreglo
+    public void BuscarElemento(int elemento)
+    {
+        if (indices.ContainsKey(elemento))
+        {
+            List<(int, int)> posiciones = indices[elemento];
+            Console.Write($"El elemento {elemento} se encuentra en los índices: ");
+            foreach (var posicion in posiciones)
+            {
+                Console.Write($"[{posicion.Item1},{posicion.Item2}] ");
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine($"El elemento {elemento} no se encuentra en el arreglo.");
+        }
+    }
+}
 
 class Program
 {
     static void Main()
     {
-        int[,] numeros = new int[3, 3];
-        numeros[0, 0] = 10;
-        numeros[0, 1] = 20;
-        numeros[0, 2] = 30;
-        numeros[1, 0] = 40;
-        numeros[1, 1] = 50;
-        numeros[1, 2] = 60;
-        numeros[2, 0] = 70;
-        numeros[2, 1] = 80;
-        numeros[2, 2] = 90;
-        int elemento = 50;
+        ArregloBidimensional arreglo = new ArregloBidimensional();
 
-        Buscar(numeros, elemento);
-    }
+        // Ingresar elementos en el arreglo
+        arreglo.IngresarElementos();
 
-    static void Buscar(int[,] numeros, int elemento)
-    {
-        for (int i = 0; i < numeros.GetLength(0); i++)
-        {
-            for (int j = 0; j < numeros.GetLength(1); j++)
-            {
-                if (numeros[i, j] == elemento)
-                {
-                    Console.WriteLine("El elemento " + elemento + " se encuentra en el índice [" + i + "," + j + "]");
-                    return;
-                }
-            }
-        }
-        Console.WriteLine("El elemento " + elemento + " no se encuentra en el arreglo.");
+        // Solicitar al usuario que ingrese el elemento a buscar
+        Console.Write("Ingrese el elemento a buscar: ");
+        int elemento = int.Parse(Console.ReadLine());
+
+        // Buscar el elemento en el arreglo
+        arreglo.BuscarElemento(elemento);
     }
 }
--- practica1/obj/Debug/net9.0/practica1.AssemblyInfo.cs
+++ practica1/obj/Debug/net9.0/practica1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practica1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc831b1a7efe4735d69dccfaec5b7db94eea6103")]
 [assembly: System.Reflection.AssemblyProductAttribute("practica1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practica1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
